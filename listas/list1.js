274ad46ab2cd59e39bc0f4fdd8eae325c586527f
greroskix <andrezanattagreroski@gmail.com>
2025-01-29 08:39:44 -0300

    Update Files

--- listas/list1.js
+++ listas/list1.js
@@ -7,6 +7,9 @@ let soma = a + b
 let sub = a - b
 let mult = a * b
 let div = a / b
-let resto = a % b;
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div);
 
 // Exercício 2: 
