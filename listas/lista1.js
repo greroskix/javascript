489e6784f91ed8d0ba04caf8d5040fcae070a783
greroskix <andrezanattagreroski@gmail.com>
2025-01-29 08:19:09 -0300

    Initial commit

---
+++ listas/lista1.js
@@ -0,0 +1,12 @@
+// Exercício 1:
+
+let a = 7
+let b = 77
+
+let soma = a + b
+let sub = a - b
+let mult = a * b
+let div = a / b
+let resto = a % b
+
+// Exercício 2: 
---
+++ operadores/aritmeticos.js
@@ -0,0 +1,30 @@
+console.log("Hello World")
+
+// Para declarar váriaveis
+
+let nome = "Juliana"
+var nomes = "Juliana"
+
+// ctrl + ; -> comentários
+
+console.log(nome)
+let idade = 24
+console.log("Minha idade é", idade, "anos")
+// concatenação = juntar texto e valor
+
+// Operações Matemáticas
+
+let a = 3
+let b = 2
+
+let soma = a + b
+let sub = a - b
+let mult = a * b
+let div = a / b
+let resto = a % b // Oque sobra da divisão
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(resto)
