209ee21fa074b545c07fdce8d32165b78e021922
greroskix <andrezanattagreroski@gmail.com>
2025-02-04 09:24:38 -0300

    Update files

--- estrutura_condicional/if.js
+++ estrutura_condicional/if.js
@@ -5,4 +5,29 @@ console.log("Sua idade é ", idade);
 
 if (idade >=18){
     console.log("Você e maior de idade")
+} else {
+    console.log("Você e menor de idade")
 }
+console.log("")
+
+// Verificando se um número é impar ou e par
+
+let numero = prompt("Escolha um número:")
+let resultado = numero % 2
+if (resultado == 0){
+    console.log("O número e par.")
+} else {
+    console.log("O número é impar")
+}
+
+// Avaliação do aluno
+
+let nota1 = prompt("Qual a primeira nota:")
+let nota2 = prompt("Qual a segunda nota:")
+let total = (nota1 + nota2) / 2
+if (total >= 7){
+    console.log("Aprovado")
+} else{
+    console.log("Reprovado")
+}
+cd
