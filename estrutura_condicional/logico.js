7534a8f837b4e46ba90968d38d270b337e42fb0b
greroskix <andrezanattagreroski@gmail.com>
2025-02-05 09:58:31 -0300

    Update Files

---
+++ estrutura_condicional/logico.js
@@ -0,0 +1,36 @@
+// Verifica se a pessoa pode votar
+
+let idade = 20
+let tituloeleitor = true
+
+if (idade >=18 && tituloeleitor == true){
+    console.log("Pode votar")
+} else {
+    console.log("Não pode votar.")
+}
+
+// Verificar se o número está dentro do intervalo
+
+let numero = 15
+if (numero >=10 && numero <= 20) {
+    console.log("O número está entre 10 e 20")
+} else {
+    console.log("O número está fora do intervalo")
+}
+
+// Comprar com desconto
+let valor = 150
+let clientevip = false
+if (valor >= 100 || cliente == true ){
+    console.log("Você ganhou frete grátis")
+} else {
+    console.log("Tem que pagar o frete")
+}
+
+// Número no intervalo
+let num = 10
+if (num >= 5 || num <= 20){
+    console.log("Funcinou")
+} else {
+    console.log("Não funciona")
+}
