08510bba85f9e7e17e9dc803f87a828e3bca4a22
greroskix <andrezanattagreroski@gmail.com>
2025-02-18 11:27:20 -0300

    Update Files list 9

---
+++ array/array.js
@@ -0,0 +1,19 @@
+let amigos = ["Carlos", "Joana", "Marcos", "Barbara", "Luana"]
+
+console.log[3]
+
+for(let i = 0; i < amigos.length; i++){
+    console.log(amigos[i])
+}
+
+console.log("Adicionando ao fim da lista")
+amigos.push("Fernanda")
+console.log(amigos)
+
+console.log("Remove o último da lista")
+amigos.pop()
+console.log(amigos)
+
+console.log("Adiciona no inicio da lista")
+amigos.unshift("Tati")
+console.log(amigos)
---
+++ laco/for.js
@@ -0,0 +1,19 @@
+for(let contador = 0; contador <= 5; contador+=1){
+    console.log(contador)
+} // Usando o for
+// No while ficaria assim
+let cont = 0
+while(cont <=5){
+    console.log(cont)
+    cont+=1
+}
+
+for(let c = 5; c >= 0; c--){
+    console.log(c)
+}
+
+let numero = 4
+for(let mult =1; mult <=5; mult+=1){
+    let resultado = mult * numero
+    console.log(resultado)
+}
